44eac17cceb93417d98426aa7581ee1f8b57edf0
jinkeipooh <k.chan.respect@gmail.com>
2021-06-18 18:02:18 +0900

    エラーメッセージの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname,  presence: true
-  validates :last_name,  presence: true
-  validates :last_name_r,  presence: true
-  validates :first_name,  presence: true
-  validates :first_name_r,  presence: true
+  validates :last_name,  presence: true, format: {
+    with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,
+    message: "全角で入力して下さい"
+  }
+  validates :last_name_r,  presence: true, format: {
+    with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,
+    message: "全角カタカナのみで入力して下さい"
+  }
+  validates :first_name,  presence: true, format: {
+    with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,
+    message: "全角で入力して下さい"
+  }
+  validates :first_name_r,  presence: true, format: {
+    with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,
+    message: "全角カタカナのみで入力して下さい"
+  }
   validates :birthday,  presence: true
 
   # validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥]/+\z }
@@ -15,3 +27,6 @@ class User < ApplicationRecord
   # validates :last_name_r, format: { with: /\p{katakana}/ }
   # validates :first_name_r, format: { with: /\p{katakana}/ }
 end
+
+
+/\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/
