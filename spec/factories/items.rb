6e9c4e0dbce3036062aae9f221f93d09e502d0e8
jinkeipooh <k.chan.respect@gmail.com>
2021-06-22 16:34:52 +0900

    テストコードのexamplesを記載

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    name          {'プーさん'}
+    text          {'ぬいぐるみですよ'}
+    category_id   {'3'}
+    condition_id  {'4'}
+    charge_id     {'4'}
+    prefecture_id {'3'}
+    day_id        {'2'}
+    price         {'5000'}
+    user_id       {'1'}
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,65 @@
 require 'rails_helper'
-
-RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+ RSpec.describe Item, type: :model do
+  before do
+    @item = FactoryBot.build(:item)
+  end
+  describe "商品出品機能" do
+    context '商品出品できるとき' do
+      it "name,text,category_id,condition_id,charge_id,prefecture_id,day_id,price,user_idが存在すれば登録できる" do
+        expect(@item).to be_valid
+      end
+      it "category_idが1以外なら登録できる" do
+      end
+      it "condition_idが1以外なら登録できる" do
+      end
+      it "charge_idが1以外なら登録できる" do
+      end
+      it "prefecture_idが1以外なら登録できる" do
+      end
+      it "day_idが1以外なら登録できる" do
+      end
+      it "priceが半角数値なら登録できる" do
+      end
+    end
+    context '商品出品できないとき' do
+      it "imageが空では登録できない" do
+      end
+      it "nameが空では登録できない" do
+      end
+      it "textが空では登録できない" do
+      end
+      it "category_idが空では登録できない" do
+      end
+      it "condition_idが空では登録できない" do
+      end
+      it "charge_idが空では登録できない" do
+      end
+      it "prefecture_idが空では登録できない" do
+      end
+      it "day_idが空では登録できない" do
+      end
+      it "priceが空では登録できない" do
+      end
+      it "user_idが空では登録できない" do
+      end
+        #  かぶってるかな？いらないか？
+      it "category_idが1だと登録できない" do
+      end
+      it "condition_idが1だと登録できない" do
+      end
+      it "charge_idが1だと登録できない" do
+      end
+      it "prefecture_idが1だと登録できない" do
+      end
+      it "day_idが1だと登録できない" do
+      end
+        #  //かぶってるかな
+      it "priceが半角でなければ登録できない" do
+      end
+      it "priceが300未満は登録できない" do
+      end
+      it "priceが9999999より大きい値は登録できない" do
+      end
+    end
+  end
+ end
